8df05da149eef2459e5628489132ee68845c941e
cyivert <phcyiver2@gmail.com>
2025-02-04 16:22:01 -0500

    Initial addBook() function: Implemented: 1. Validates book ID if it is duplicated or else display error message. 2. newBook dynamically allocates to heap using malloc 3. Checks if head node is NULL then it will be set to head else not NULL then will be set to newBook (or the next book end of the list)
    Not added:
    1. User prompt.

--- Assignment1-Group4/Assignment1-Group4/LibraryCatalog.c
+++ Assignment1-Group4/Assignment1-Group4/LibraryCatalog.c
@@ -20,29 +20,80 @@ typedef struct Book
 
 // List of prototypes
 void addBook(Book** head, int id, const char* title, const char* author, int publication_year);
-void viewBooks(Book* head);
-void updateBook(Book* head, int id);
-void deleteBook(Book** head, int id);
-void searchBooks(Book* head, const char* title);
+//void viewBooks(Book* head);
+//void updateBook(Book* head, int id);
+//void deleteBook(Book** head, int id);
+//void searchBooks(Book* head, const char* title);
+
+// List of pointers:
+// Book* head --> head node
+// Book* current --> current node
+// Book* newBook --> new book node
 
 int main(void) 
 {
+	// HEAD NODE (first book in the linked list)
+	Book* head = NULL; // head node is set to NULL (must)
+
+	// user prompt to add a book
+
 	return 0;
 }
 
 //
-// FUNCTION :
+// FUNCTION : addBook()
 // DESCRIPTION :
+// This function will add a new book to the linked list. As well as to check if the book id already exists.
 // PARAMETERS : 
-// RETURNS :
+// Book** head - pointer pointing to the head of the linked list
+// int id - the id of the book
+// const char* title - the title of the book
+// const char* author - the author of the book
+// int publication_year - the publication year of the book
+// RETURNS : n/a (void)
 //
 void addBook(Book** head, int id, const char* title, const char* author, int publication_year) 
 {
+	// this will be to identify the current book in the linked list making sure it has a value
+	Book* current = *head; // current points to the head of the linked list (the first book in the list)
 
+	// iterate through the linked list to check if the book id already exists
+	while (current != NULL)
+	{
+		if (current->id == id) // if the current book id is equal to the id passed in (passed in = the value that is passed into the function)
+		{
+			printf("Book ID of %d, exist arleady. \n", id); // print out that the book with the id already exists
+			return; 
+		}
+	}
+
+	// Book pointer to newBook, malloc is used to allocate memory for the new book to the heap
+	Book* newBook = (Book*)malloc(sizeof(Book));
+
+	// the book details
+	newBook->id = id; // the newBook id is set to id which is passed in.
+	strcpy(newBook->title, title); // the newBook title is set to title which is passed in.
+	strcpy(newBook->author, author); // the newBook author is set to author which is passed in.
+	newBook->publication_year = publication_year; // the newBook publication_year is set to publication_year which is passed in.
+	newBook->next = NULL; // the newBook next is set to NULL
+
+	// checks if next book HEAD NODE is NULL
+	if (*head == NULL)
+	{
+		*head = newBook; // then the head node is set to the newBook
+	}
+	else // if the next book HEAD NODE is not NULL
+	{
+		while (current->next != NULL)
+		{
+			current = current->next; // then the current (data) is set to the next book
+		}
+		current->next = newBook; // the current next is set to the newBook
+	}
 }
 
 //
-// FUNCTION : 
+// FUNCTION : viewBooks()
 // DESCRIPTION :
 // PARAMETERS : 
 // RETURNS :
@@ -53,7 +104,7 @@ void viewBooks(Book* head)
 }
 
 //
-// FUNCTION : 
+// FUNCTION :  updateBook()
 // DESCRIPTION :
 // PARAMETERS : 
 // RETURNS :
@@ -64,7 +115,7 @@ void updateBook(Book* head, int id)
 }
 
 //
-// FUNCTION : 
+// FUNCTION : deleteBook()
 // DESCRIPTION :
 // PARAMETERS : 
 // RETURNS :
@@ -75,7 +126,7 @@ void deleteBook(Book** head, int id)
 }
 
 //
-// FUNCTION : 
+// FUNCTION : searchBooks()
 // DESCRIPTION :
 // PARAMETERS : 
 // RETURNS :
