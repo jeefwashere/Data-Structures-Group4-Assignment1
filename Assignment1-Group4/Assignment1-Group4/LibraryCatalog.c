2ee664b98e1878dc216ec9e00cc3be756e4248eb
Jeff Tieng <jtieng1377@conestogac.on.ca>
2025-02-09 01:13:44 -0500

    Removed temporary add new student function

--- Assignment1-Group4/Assignment1-Group4/LibraryCatalog.c
+++ Assignment1-Group4/Assignment1-Group4/LibraryCatalog.c
@@ -32,51 +32,6 @@ int main(void) {
 
 void addBook(Book** head, int id, const char* title, const char* author, int publication_year) {
 
-	// this will be to identify the current book in the linked list making sure it has a value
-	Book* current = *head; // current points to the head of the linked list (the first book in the list)
-
-	// iterate through the linked list to check if the book id already exists
-	while (current != NULL)
-	{
-		if (current->id == id) // if the current book id is equal to the id passed in (passed in = the value that is passed into the function)
-		{
-			printf("Book ID of %d, exists already. \n", id); // print out that the book with the id already exists
-			return;
-		}
-
-		current = current->next;
-	}
-
-	// Book pointer to newBook, malloc is used to allocate memory for the new book to the heap
-	Book* newBook = (Book*)malloc(sizeof(Book));
-	// check if the newBook is NULL (in other words, to see newBook exists or not in the heap)
-	if (newBook == NULL) // if the newBook is NULL
-	{
-		printf("Failed to allocate memory. \n"); // print out that the memory allocation failed
-		return;
-	}
-
-	// the book details
-	newBook->id = id; // the newBook id is set to id which is passed in.
-	strcpy_s(newBook->title, sizeof(newBook->title), title); // the newBook title is set to title which is passed in.
-	strcpy_s(newBook->author, sizeof(newBook->author), author); // the newBook author is set to author which is passed in.
-	newBook->publication_year = publication_year; // the newBook publication_year is set to publication_year which is passed in.
-	newBook->next = NULL; // the newBook next is set to NULL
-
-	// this sends the new book to the end of the linked list*
-	// checks if next book HEAD NODE is NULL
-	if (*head == NULL)
-	{
-		*head = newBook; // then the head node is set to the newBook-
-	}
-	else {
-		// Otherwise, find the end of the list and add the new book there
-		current = *head;
-		while (current->next != NULL) {
-			current = current->next;
-		}
-		current->next = newBook;
-	}
 }
 
 void viewBooks(Book* head) {
